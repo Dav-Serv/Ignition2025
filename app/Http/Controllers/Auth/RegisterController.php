ff2f7d1f3c5dc058337cb005abbf98aca0a5e1d4
dav-serv <dav.1234rp@gmail.com>
2026-01-05 22:32:36 +0700

    admin

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -87,7 +87,8 @@ class RegisterController extends Controller
             'no_tlp'        => $request->no_tlp,
             'foto'          => $imagePath,
             'email'         => $request->email,
-            'password'      => Hash::make($request->password)
+            'password'      => Hash::make($request->password),
+            'role'        => 'admin'
         ]);
 
         $login =[
